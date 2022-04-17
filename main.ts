3d6c26ec10ede2e7af0c60032a1ca4d8b05dc80f
luravi <61315321+luravi@users.noreply.github.com>
2022-04-17 11:33:20 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,10 +12,17 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 5; index++) {
+        music.playTone(784, music.beat(BeatFraction.Breve))
         music.playTone(880, music.beat(BeatFraction.Breve))
+        music.playTone(988, music.beat(BeatFraction.Breve))
+        music.playTone(523, music.beat(BeatFraction.Breve))
+        music.playTone(784, music.beat(BeatFraction.Breve))
+        music.playTone(698, music.beat(BeatFraction.Breve))
+        music.playTone(659, music.beat(BeatFraction.Breve))
+        music.playTone(587, music.beat(BeatFraction.Breve))
+        music.playTone(523, music.beat(BeatFraction.Breve))
     }
 })
-music.playMelody("C D E D C D E F ", 500)
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
 })
